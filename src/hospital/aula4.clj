d53861524bca0609a8c7b252a5b00ea0e0073666
Maxwel <maxpdr96@gmail.com>
2020-07-10 19:20:34 -0300

    Study Juxt

---
+++ src/hospital/aula4.clj
@@ -0,0 +1,27 @@
+(ns hospital.aula4
+  (:use [clojure pprint])
+  (:require [hospital.model :as h.model]
+            [hospital.logic :as h.logic]))
+
+
+(defn chega-em!
+  [hospital pessoa]
+  (swap! hospital h.logic/chega-em :espera pessoa))
+
+(defn transfere!
+  [hospital de para]
+  (swap! hospital h.logic/transfere de para))
+
+(defn simula-um-dia []
+  (let [hospital (atom (h.model/new-hospital))]
+    (chega-em! hospital "joão")
+    (chega-em! hospital "maria")
+    (chega-em! hospital "daniela")
+    (chega-em! hospital "guilherme")
+    (transfere! hospital :espera :laboratorio1)
+    (pprint hospital))
+
+  )
+
+
+(simula-um-dia)
--- src/hospital/logic.clj
+++ src/hospital/logic.clj
@@ -3,18 +3,18 @@
 (defn cabe-na-fila?
   [hospital departamento]
   (-> hospital
-  (get ,,, departamento)
-  count ,,,
-  (< ,,, 5)))
+      (get,,, departamento)
+      count,,,
+      (<,,, 5)))
 
 
 
 
 (defn chega-em
   [hospital departamento pessoa]
-    (if (cabe-na-fila? hospital departamento)
-      (update hospital departamento conj pessoa)
-      (throw (ex-info "Fila ja está cheia" {:tentando-adicionar pessoa}))))
+  (if (cabe-na-fila? hospital departamento)
+    (update hospital departamento conj pessoa)
+    (throw (ex-info "Fila ja está cheia" {:tentando-adicionar pessoa}))))
 
 (defn chega-em-pausado
   [hospital departamento pessoa]
@@ -22,7 +22,7 @@
   (if (cabe-na-fila? hospital departamento)
     (do
       ;(Thread/sleep (* (rand) 2000))
-    (update hospital departamento conj pessoa))
+      (update hospital departamento conj pessoa))
     (throw (ex-info "Fila ja está cheia" {:tentando-adicionar pessoa}))))
 
 (defn chega-em-pausado-logando
@@ -41,3 +41,33 @@
 (defn atende
   [hospital departamento]
   (update hospital departamento pop))
+
+(defn atende-completo
+  [hospital departamento]
+  {:paciente (update hospital departamento peek)
+   :fila     (update hospital departamento pop)})
+
+(defn atende-completo-chama-ambos
+  [hospital departamento]
+  (let [fila (get hospital departamento)
+        peek-pop (juxt peek pop)
+        [pessoa fila-atualizada] (peek-pop fila)
+        hospital-atualizado (update hospital assoc departamento fila-atualizada)]
+    {:paciente pessoa
+     :hospital hospital-atualizado}))
+
+(defn proxima
+  "Retorna o proximo paciente da fila"
+  [hospital departamento]
+  (-> hospital
+      departamento
+      peek)
+
+
+  (defn transfere
+    "Transfere o proximo paciente da fila de para a fila para"
+    [hospital de para]
+    (let [pessoa (proxima hospital de)]
+      (-> hospital
+          (atende de)
+          (chega-em para pessoa)))))
